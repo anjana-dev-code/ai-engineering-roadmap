82e3f349251d887a535c63af1051091a8a75e52e
Anjana Chaudhari <315758556+anjana-dev-code@users.noreply.github.com>
2026-09-08 23:48:45 +0530

    SVM classifier with linear kernel on car/credit dataset

---
+++ 03_classical_ml/svm_intro.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+
+data = {
+    'owns_car': [1,1,1,1,0,0,0,0],
+    'high_credit': [1,1,0,0,1,1,0,0],
+    'defaulted': [0,0,0,1,1,1,1,1]
+}
+
+df = pd.DataFrame(data)
+
+X = df[['owns_car', 'high_credit']]
+y = df['defaulted']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size= 0.25, random_state= 42
+)
+
+clf = SVC(kernel='linear',random_state= 42 )
+clf.fit(X_train, y_train)
+
+predictions = clf.predict(X_test)
+print("Predictions:", predictions)
+print("Actual:", y_test.values) 
