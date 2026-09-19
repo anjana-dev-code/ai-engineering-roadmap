91f575058579efec07eb6ad99518c11989efcbb0
Anjana Chaudhari <315758556+anjana-dev-code@users.noreply.github.com>
2026-09-19 23:29:31 +0530

    SQL practice of Topic 1 to 7

---
+++ 04_sql/sql_prac8.py
@@ -0,0 +1,64 @@
+import sqlite3
+
+conn = sqlite3.connect("company.db")
+cursor = conn.cursor()
+
+cursor.execute("DROP TABLE IF EXISTS employees")
+cursor.execute("DROP TABLE IF EXISTS departments")
+
+cursor.execute("""
+CREATE TABLE departments(
+    dept_id INTEGER PRIMARY KEY,
+    dept_name TEXT NOT NULL
+)
+""")
+
+cursor.execute("""
+CREATE TABLE employees(
+   emp_id INTEGER PRIMARY KEY,
+   name TEXT NOT NULL,
+   salary INTEGER,
+   dept_id INTEGER,
+   FOREIGN KEY (dept_id) REFERENCES departments(dept_id)
+)
+""")
+
+cursor.execute("INSERT INTO departments VALUES (1, 'Engineering')")
+cursor.execute("INSERT INTO departments VALUES (2, 'Business')")
+cursor.execute("INSERT INTO departments VALUES (3, 'Marketing')")
+
+cursor.execute("INSERT INTO employees VALUES (1, 'Anna', 70000, 1)")
+cursor.execute("INSERT INTO employees VALUES (2, 'Viky', 65000, 1)")
+cursor.execute("INSERT INTO employees VALUES (3, 'Max', 60000, 2)")
+cursor.execute("INSERT INTO employees VALUES (4, 'Lina', 50000, 3)")
+cursor.execute("INSERT INTO employees VALUES (5, 'David', 40000, 2)")
+cursor.execute("INSERT INTO employees VALUES (7, 'Sam', 55000, 3)")
+cursor.execute("INSERT INTO employees VALUES (8, 'Priya', 80000, 1)")
+cursor.execute("INSERT INTO employees VALUES (9, 'Ali', 45000, 3)")
+
+conn.commit()
+print("Company database created")
+
+cursor.execute("SELECT * FROM departments")
+print(cursor.fetchall())
+
+cursor.execute("SELECT * FROM employees")
+print(cursor.fetchall())
+
+# Problem 1:Get the names of all employees earning more than 55000, sorted from highest to lowest salary.
+cursor.execute("""
+SELECT name 
+FROM employees
+WHERE salary > 55000           
+ORDER BY salary DESC; 
+""")
+print(cursor.fetchall())
+
+# Problem 2:Find all employees whose name starts with the letter 'A' or 'S'.
+cursor.execute("""
+SELECT name FROM employees
+WHERE name LIKE 'A%' OR name LIKE 'S%'
+ORDER BY name;
+""")
+print(cursor.fetchall())
+
