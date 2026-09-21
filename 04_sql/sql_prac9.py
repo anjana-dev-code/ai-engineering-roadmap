7c767839dcea1d89fc985d815feb5f10d1ef535a
Anjana Chaudhari <315758556+anjana-dev-code@users.noreply.github.com>
2026-09-21 23:17:45 +0530

    Topic 8 - JOIN

---
+++ 04_sql/sql_prac9.py
@@ -0,0 +1,61 @@
+import sqlite3
+
+conn = sqlite3.connect("school.db")
+cursor = conn.cursor()
+
+cursor.execute("DROP TABLE IF EXISTS enrollments")
+cursor.execute("DROP TABLE IF EXISTS students")
+cursor.execute("DROP TABLE IF EXISTS courses")
+
+cursor.execute("""
+CREATE TABLE students (
+    student_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    major TEXT
+)
+""")
+
+cursor.execute("""
+CREATE TABLE courses (
+    course_id INTEGER PRIMARY KEY,
+    course_name TEXT NOT NULL,
+    credits INTEGER
+)
+""")
+
+cursor.execute("""
+CREATE TABLE enrollments (
+    enrollment_id INTEGER PRIMARY KEY,
+    student_id INTEGER,
+    course_id INTEGER,
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+    )
+""")
+
+# Students
+cursor.execute("INSERT INTO students VALUES (1, 'Anjana', 'ICT')")
+cursor.execute("INSERT INTO students VALUES (2, 'Raj', 'Business')")
+cursor.execute("INSERT INTO students VALUES (3, 'Jack', 'ICT')")
+
+# Courses
+cursor.execute("INSERT INTO courses VALUES (101, 'Databases', 3 )")
+cursor.execute("INSERT INTO courses VALUES (102, 'Networking', 4 )")
+cursor.execute("INSERT INTO courses VALUES (103, 'Marketing', 3 )")
+
+# Enrollments
+cursor.execute("INSERT INTO enrollments VALUES (1, 1, 101)")
+cursor.execute("INSERT INTO enrollments VALUES (2, 1, 102)")
+cursor.execute("INSERT INTO enrollments VALUES (3, 2, 103)")
+
+conn.commit()
+print("Joins practice data ready")
+
+cursor.execute("SELECT * FROM students")
+print(cursor.fetchall())
+
+cursor.execute("SELECT * FROM courses")
+print(cursor.fetchall())
+
+cursor.execute("SELECT * FROM enrollments")
+print(cursor.fetchall())
