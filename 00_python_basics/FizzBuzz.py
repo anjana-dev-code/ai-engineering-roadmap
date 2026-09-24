160b81ac984a6687f5f3102a618e23a2db0778cc
Anjana Chaudhari <315758556+anjana-dev-code@users.noreply.github.com>
2026-09-24 20:13:18 +0530

    Basic FizzBuzz problem solving for practice

---
+++ 00_python_basics/FizzBuzz.py
@@ -0,0 +1,21 @@
+"""Problem 2 (Basic)
+
+Write a program that prints all numbers from 1 to 50, but:
+
+for multiples of 3, print "Fizz" instead of the number
+for multiples of 5, print "Buzz" instead of the number
+for numbers that are multiples of both 3 and 5, print "FizzBuzz" """
+
+for num in range(1,51):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+    
+
+
+
